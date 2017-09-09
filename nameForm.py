837f64da5e3238f2c1856b6c6901d06b6a2ec3d2
Felipe <felipejmesina@gmail.com>
2017-09-08 19:04:34 -0700

    completed assignment

---
+++ nameForm.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, request
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("nameForm.html")
+
+@app.route("/process", methods=['POST'])
+def process_form():
+    print request.form
+    return redirect("/")
+
+
+
+
+
+
+app.run(debug=True)
